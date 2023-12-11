3f67a09a6d3823bf61c585823cd22bd133216734
kannstas <k.annstas@gmail.com>
2023-12-11 15:26:04 +0300

    First commit

---
+++ src/main/java/ru/nastya/springcourse/MyPersonSpringMvcAppApplication.java
@@ -0,0 +1,13 @@
+package ru.nastya.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonSpringMvcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonSpringMvcAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nastya/springcourse/MyPersonSpringMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nastya.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonSpringMvcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
